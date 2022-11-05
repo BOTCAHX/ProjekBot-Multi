c7b68e625f073a23c67b3358ef5282700e9ce0e9
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-05 12:55:16 +0700

    add setting

--- setting.js
+++ setting.js
@@ -1,1 +1,110 @@
-[]
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+/**Bot Bersama Ya owner nya banyak**/
+global.owner = [
+  ['6282221792667', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],
+  ['6281795861695', 'Owner', true],  
+  ['6281795861695'],
+  ['6281795861695'],
+  ['6281795861695'],
+  ['6281795861695'],
+  ['6281795861695'],
+  ['6281795861695']
+] 
+global.suittag = ['6282221792667'] 
+global.prems = ['6282221792667'] 
+
+global.packname = 'botwa'
+global.author = 'wmbot'
+global.wm = 'botwa'
+global.igfg = 'botwa'
+global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+
+global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
+global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
+global.lolkeysapi = ['85faf717d0545d14074659ad']
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  adiisus: 'https://adiixyzapi.herokuapp.com',
+  lol: 'https://api.lolhuman.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  Velgrynd: 'https://velgrynd.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  hardianto: 'http://hardianto-chan.herokuapp.com',
+  shadow: 'https://api.reysekha.xyz',
+  apialc: 'https://api-alc.herokuapp.com',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  ana: 'https://anabotofc.herokuapp.com/',
+  kanx: 'https://kannxapi.herokuapp.com/',
+  dhnjing: 'https://dhnjing.xyz',
+  violetics: 'https://violetics.pw'
+},
+global.APIKeys = { // Apiekylu tot
+  // 'https://website': 'apikey'
+  'https://api-alc.herokuapp.com': 'ConfuMods',
+  'https://api.reysekha.xyz': 'apirey',
+  'https://melcanz.com': 'F3bOrWzY',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://api.xteam.xyz': '5bd33b276d41d6b4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.me': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.lolhuman.xyz': '9b817532fadff8fc7cb86862',
+  'https://botstyle-api.herokuapp.com': 'Eyar749L',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://anabotofc.herokuapp.com/': 'AnaBot',
+  'https://violetics.pw': 'beta'
+}
+//pake ini bila perlu ini cuma sekedar template image
+global.imagen1 = fs.readFileSync('./bot1.jpg')
+global.imagen2 = fs.readFileSync('./src/bot.jpg') 
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
+global.imagen4 = fs.readFileSync('./bot3.png')
+
+/**  Ini si tergantung mau di pake atau engga
+global.htki = '*⭑•̩̩͙⊱•••• ☪*'
+global.htka = '*☪ ••••̩̩͙⊰•⭑*'
+
+global.cmenut = '❖––––––『'
+global.cmenub = '┊✦ '
+global.cmenuf = '╰━═┅═━––––––๑\n'
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
+**/
+global.mods = [] 
+
+global.multiplier = 69
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'setting.js'"))
+import(`${file}?update=${Date.now()}`)})
