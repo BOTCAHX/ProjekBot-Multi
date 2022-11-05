878c12cddc078b9351dcd51a67c12e5ca4bdd2d2
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-05 14:35:51 +0700

    cek

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
 logger: P({ level: 'silent'}),
-browser: ['Rtxzy','Safari','1.0.0']
+browser: ['RTXZY','Safari','1.0.0']
 }
 
 global.conn = makeWASocket(connectionOptions)
