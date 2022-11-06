d099b169fe7295e760884bb0e0da96acd75581b6
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2022-11-06 09:03:21 +0700

    .

--- handler.js
+++ handler.js
@@ -1,1 +1,17 @@
-[]
+import { smsg } from './lib/simple.js'
+import { format } from 'util'
+import { fileURLToPath } from 'url'
+import path, { join } from 'path'
+import { unwatchFile, watchFile } from 'fs'
+import chalk from 'chalk'
+
+/**
+ * @type {import('@adiwajshing/baileys')}
+ */
+const { proto } = (await import('@adiwajshing/baileys')).default
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+**/
